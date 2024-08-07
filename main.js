deb73fc25c9a422cafdffc8ec84dfbd7d9b62f1b
Jack Bogart <jackbogart0822@gmail.com>
2024-08-06 23:39:20 -0400

    Add remaining BST methods
    These methods mostly involved traversal.

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
 import Tree from './tree.js';
 
-const test = new Tree([5, 3, 2, 7, 8, 7, 75, 32, 7, 1, 0]);
-// const test = new Tree([1, 2, 3]);
+const test = new Tree([1, 2, 3, 4, 5, 6, 7, 75]);
+console.log(test.isBalanced());
+// const test = new Tree([]);
 
 test.prettyPrint(test.root);
 
 // test.insert(5);
-// test.insert(5);
+test.insert(-1);
+test.insert(-2);
+test.insert(-3);
+console.log(test.isBalanced());
 
-test.deleteItem(2);
+// test.deleteItem(2);
+test.prettyPrint(test.root);
+function specialLog(node) {
+  process.stdout.write(`${node.value} `);
+}
+test.prettyPrint(test.find(1));
+test.levelOrder(specialLog);
+console.log('\n');
+test.inOrder(specialLog);
+console.log('\n');
+test.preOrder(specialLog);
+console.log('\n');
+test.postOrder(specialLog);
+console.log('\n');
+
+const testingNode = 75;
+console.log(test.height(test.find(testingNode)));
+console.log(test.depth(test.find(testingNode)));
+
+test.rebalance();
 test.prettyPrint(test.root);
--- tree.js
+++ tree.js
@@ -16,21 +16,13 @@ export default class Tree {
   #buildTreeRecursively(array, start, end) {
     if (start > end) return null;
 
-    const mid = Math.floor((start + end) / 2);
+    const mid = Math.ceil((start + end) / 2);
     const node = new Node(array[mid]);
 
     node.left = this.#buildTreeRecursively(array, start, mid - 1);
     node.right = this.#buildTreeRecursively(array, mid + 1, end);
 
     return node;
-  }
-
-  insert(value) {
-    if (this.root === null) {
-      this.root = new Node(value);
-    } else {
-      this.root = this.#insertRecursively(value, this.root);
-    }
   }
 
   #insertRecursively(value, node) {
@@ -47,8 +39,16 @@ export default class Tree {
     return node;
   }
 
-  deleteItem(value) {
-    this.root = this.#deleteItemRecursively(value, this.root);
+  insert(value) {
+    this.root = this.#insertRecursively(value, this.root);
+  }
+
+  #getMinimumNode(node) {
+    while (node.left !== null) {
+      node = node.left;
+    }
+
+    return node;
   }
 
   #deleteItemRecursively(value, node) {
@@ -68,11 +68,173 @@ export default class Tree {
         return node.left;
       }
 
-      const newNode = getMinimumNode(node.right);
+      const newNode = this.#getMinimumNode(node.right);
       node.value = newNode.value;
       node.right = this.#deleteItemRecursively(newNode.value, node.right);
     }
     return node;
+  }
+
+  deleteItem(value) {
+    this.root = this.#deleteItemRecursively(value, this.root);
+  }
+
+  #findRecursively(value, node) {
+    if (node === null) {
+      return null;
+    }
+
+    if (value < node.value) {
+      return this.#findRecursively(value, node.left);
+    } else if (value > node.value) {
+      return this.#findRecursively(value, node.right);
+    }
+    return node;
+  }
+
+  find(value) {
+    return this.#findRecursively(value, this.root);
+  }
+
+  levelOrder(callback) {
+    if (this.root === null) return;
+    if (typeof callback !== 'function') {
+      throw new Error('A callback function is required');
+    }
+
+    const queue = [this.root];
+
+    while (queue.length !== 0) {
+      const node = queue.shift();
+
+      callback(node);
+
+      if (node.left !== null) {
+        queue.push(node.left);
+      }
+
+      if (node.right !== null) {
+        queue.push(node.right);
+      }
+    }
+  }
+
+  #inOrderRecursively(callback, node) {
+    if (node === null) {
+      return;
+    }
+
+    this.#inOrderRecursively(callback, node.left);
+    callback(node);
+    this.#inOrderRecursively(callback, node.right);
+  }
+
+  inOrder(callback) {
+    if (this.root === null) return;
+    if (typeof callback !== 'function') {
+      throw new Error('A callback function is required');
+    }
+
+    this.#inOrderRecursively(callback, this.root);
+  }
+
+  #preOrderRecursively(callback, node) {
+    if (node === null) {
+      return;
+    }
+
+    callback(node);
+    this.#preOrderRecursively(callback, node.left);
+    this.#preOrderRecursively(callback, node.right);
+  }
+
+  preOrder(callback) {
+    if (this.root === null) return;
+    if (typeof callback !== 'function') {
+      throw new Error('A callback function is required');
+    }
+
+    this.#preOrderRecursively(callback, this.root);
+  }
+
+  #postOrderRecursively(callback, node) {
+    if (node === null) {
+      return;
+    }
+
+    this.#postOrderRecursively(callback, node.left);
+    this.#postOrderRecursively(callback, node.right);
+    callback(node);
+  }
+
+  postOrder(callback) {
+    if (this.root === null) return;
+    if (typeof callback !== 'function') {
+      throw new Error('A callback function is required');
+    }
+
+    this.#postOrderRecursively(callback, this.root);
+  }
+
+  height(node) {
+    if (node === null) return -1;
+
+    const left = this.height(node.left);
+    const right = this.height(node.right);
+
+    return Math.max(left, right) + 1;
+  }
+
+  #depthRecursively(targetNode, currentNode, depth) {
+    if (currentNode === null) return -1;
+    else if (targetNode.value < currentNode.value) {
+      return this.#depthRecursively(targetNode, currentNode.left, depth + 1);
+    } else if (targetNode.value > currentNode.value) {
+      return this.#depthRecursively(targetNode, currentNode.right, depth + 1);
+    }
+
+    return depth;
+  }
+
+  depth(node) {
+    if (node === null) return -1;
+
+    return this.#depthRecursively(node, this.root, 0);
+  }
+
+  #isBalancedRecursively(node) {
+    if (node === null) {
+      return 0;
+    }
+
+    const left = this.#isBalancedRecursively(node.left);
+    if (left === -1) {
+      return -1;
+    }
+
+    const right = this.#isBalancedRecursively(node.right);
+    if (right === -1) {
+      return -1;
+    }
+
+    if (Math.abs(left - right) > 1) return -1;
+
+    return Math.max(left, right) + 1;
+  }
+
+  isBalanced() {
+    return this.#isBalancedRecursively(this.root) !== -1;
+  }
+
+  rebalance() {
+    const array = [];
+
+    function fillArray(node) {
+      array.push(node.value);
+    }
+
+    this.inOrder(fillArray);
+    this.root = this.buildTree(array);
   }
 
   prettyPrint(node, prefix = '', isLeft = true) {
@@ -91,12 +253,4 @@ export default class Tree {
       this.prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
     }
   }
-}
-
-function getMinimumNode(node) {
-  while (node.left !== null) {
-    node = node.left;
-  }
-
-  return node;
 }
